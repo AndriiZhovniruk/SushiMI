6c7c7a1e760f236c87d540dd107ada5cdb1325f8
Andrii Zhovniruck <zhovniruk_ak21@nuwm.edu.ua>
2023-12-03 17:23:42 +0200

    Міграції
    Виконав міграції для створення таблиці "страви", та обновив базу даних

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -17,7 +17,7 @@ namespace Sushimi.Data
     {
 		public int Id { get; set; }
 		public string Name { get; set; }
-		public string Description { get; set; }
+		public string Weight { get; set; }
 		public int Price { get; set; }
     }
 }
--- Data/Migrations/20231203152151_AddDish.Designer.cs
+++ Data/Migrations/20231203152151_AddDish.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Sushimi.Data;
 
@@ -11,9 +12,11 @@ using Sushimi.Data;
 namespace Sushimi.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231203152151_AddDish")]
+    partial class AddDish
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
