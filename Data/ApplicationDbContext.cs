8f32ef4f932d58c4266be3b9fe8cdcd5cbf5f8ad
Andrii Zhovniruck <zhovniruk_ak21@nuwm.edu.ua>
2023-12-03 17:20:28 +0200

    Створив базу даних

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -5,9 +5,19 @@ namespace Sushimi.Data
 {
 	public class ApplicationDbContext : IdentityDbContext
 	{
-		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+		public DbSet<Dish> Dish { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
 			: base(options)
 		{
 		}
 	}
+
+    public class Dish
+    {
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public string Description { get; set; }
+		public int Price { get; set; }
+    }
 }
